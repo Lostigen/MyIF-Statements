abd73f918c99760d4e185f6eda592df7d5c7da15
Lostigen <jimmy.lostigen@outlook.com>
2026-04-14 19:26:43 +0200

    System repair

--- obj/Debug/net10.0/MyIF-Statements.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyIF-Statements.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyIF-statements")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6370fa1bfef145570a990c958d8c2c8403e69ab3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba66e44646e53e22449d805b138add3665641af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyIF-statements")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyIF-statements")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
